80d645b8a7d356678081a1767a0cc39e761da9b1
Ingyu Bang <ingyu1008@gmail.com>
2023-04-16 11:46:07 +0000

    기출문제유형01회 제출

---
+++ 기출문제/기출문제유형01회/답안 제출 파일/javascript/script.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function(){
+
+    $(".navi > li").mouseover(function(){
+        $(this).find(".submenu").stop().slideDown(500);
+    }).mouseout(function(){
+        $(this).find(".submenu").stop().slideUp(500);
+    })
+
+    $(".imgslide a:gt(0)").hide();
+    setInterval(function(){
+        $(".imgslide a:first-child").fadeOut(1000)
+        .next('a').fadeIn(1000).end()
+        .appendTo(".imgslide");
+    }, 3000);
+
+    $(".notice li:first").click(function(){
+        $(".modal").attr("active", "");
+    });
+    $(".btn").click(function(){
+        $(".modal").removeAttr("active");
+    })
+})
